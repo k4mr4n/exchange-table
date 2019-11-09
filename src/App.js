c9f782b4efddd1a95253d53c019c3b4800fdff56
Kamran Ghoochanian <kamran.ghoochanian@gmail.com>
2019-11-09 17:25:04 -0500

    hot reloading fixed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import { Provider } from 'react-redux'
 
-import createStore from './redux'
 import HomePageView from './views/homepage.view'
 
 function App () {
-  const store = createStore()
   return (
-    <Provider store={store}>
+    <>
       <CssBaseline />
       <HomePageView />
-    </Provider>)
+    </>)
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App'
+import createStore from './redux'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore()
+
+const render = Component => {
+  return ReactDOM.render(
+    <Provider store={store}>
+      <Component />
+    </Provider>,
+    document.getElementById('root')
+  )
+}
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default
+    render(NextApp)
+  })
+}
